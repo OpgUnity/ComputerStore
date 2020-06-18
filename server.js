2aeab97caf928e7a83da65d6a705062059dcc760
Ivan <eroshin_01@mail.ru>
2020-06-19 01:32:55 +0300

    Провёл работы по улучшению серверной части, внёс структуру проекта в ридми

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-var cors = require('cors')
+const cors = require('cors');
 
-var express = require('express'),
+const express = require('express'),
     app = express(),
     port = process.env.PORT || 3002,
     bodyParser = require('body-parser');
-;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var routes = require('./server/routes/routes');
+const routes = require('./server/routes/routes');
 routes(app);
 
 app.listen(port);
 
-console.log('todo list RESTful API server started on: ' + port);
+console.log('todo list RESTful API server started on: http://localhost:' + port);
 
