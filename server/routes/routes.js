e92c1faedbc6bfb457b912a21a09e5873196162d
bagrova-nastya <fefhwjfbwejhf@gmail.com>
2020-06-22 15:59:49 +0300

    Создан контроллер для таблицы product_category. Добавлены маршруты таблицы в routes.js.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -7,11 +7,7 @@ module.exports = function (app) {
 
     let conditionsController = require('../controllers/conditionsController');
     let salesController = require('../controllers/salesController');
-    let salesProductsController = require('../controllers/salesProductsController');
     let productCategoryController = require('../controllers/productCategoryController');
-    let order_statesController = require('../controllers/order_statesController');
-    let ordersController = require('../controllers/ordersController');
-    let warehouseController = require('../controllers/warehouseController');
 
     app.route('/login')
         .post(conditionsController.login);
@@ -36,57 +32,14 @@ module.exports = function (app) {
     app.route('/conditions/:id')
         .delete(conditionsController.delete);
 
-    app.route('/order_states')
-        .get(order_statesController.get)
-        .put(order_statesController.put)
-        .post(order_statesController.update);
-
-    app.route('/orders')
-        .get(ordersController.get)
-        .put(ordersController.put)
-        .post(ordersController.update);
-
-    app.route('/warehouse')
-        .get(warehouseController.get)
-        .put(warehouseController.put)
-        .post(warehouseController.update);
-
     app.route('/sales/:id')
         .delete(salesController.delete);
 
     app.route('/product_category/:id')
         .delete(productCategoryController.delete);
 
-    app.route('/sales_products')
-        .get(salesProductsController.get)
-        .put(salesProductsController.put)
-        .post(salesProductsController.update);
-
-    app.route('/sales_products/:id')
-        .delete(salesProductsController.delete)
-
-    app.route('/order_states/:id')
-        .delete(order_statesController.delete)
-
-    app.route('/orders/:id')
-        .delete(ordersController.delete)
-
-    app.route('/warehouse/:id')
-        .delete(warehouseController.delete)
-
-
-    app.route('/sales_products')
-        .get(salesProductsController.get)
-        .put(salesProductsController.put)
-        .post(salesProductsController.update);
-
-    app.route('/sales_products/:id')
-        .delete(salesProductsController.delete)
-
     //Если мы исползьуем делит, то можем в адресной строке сказать, какой ид нужно удалить
     //URL выглядит так:http://localhost:3002/conditions/1 с методом DELETE
 
-    app.route('/conditions/:id')
-        .delete(conditionsController.delete);
 
 };
