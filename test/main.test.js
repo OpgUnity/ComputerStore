6dd0763611c051d81c7982c588bf72abdf8ef4d3
Oleg <gg@gmail.com>
2020-06-22 12:21:30 +0300

    часть тестов QuaryBilders

---
+++ test/main.test.js
@@ -0,0 +1,28 @@
+const {bodyNormalisator} = require('../server/utils');
+test('String', () => {
+    expect(bodyNormalisator({
+        condition_name: "test",
+        condition_description: "test1"
+    })).toEqual({
+        condition_name: "test",
+        condition_description: "test1"
+    });
+
+});
+
+const {selectQueryBuilder} = require('../server/queryBuilders');
+test('Select', () => {
+    expect(selectQueryBuilder("Green")).toEqual("select * from Green");
+});
+
+const {appendWhere} = require('../server/queryBuilders');
+test('Where', () => {
+    expect(appendWhere("Green",["black","orange"])).toEqual("Green WHERE black orange");
+});
+
+
+const {appendOrderBy} = require('../server/queryBuilders');
+test('Order by', () => {
+    expect(appendOrderBy("Green","black")).toEqual("Green ORDER BY black");
+});
+
