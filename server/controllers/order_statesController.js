76330f019e1578484ae7aa7792f28f99d6be786c
Ekaterina <ased2@mail.ru>
2020-06-26 12:28:05 +0300

    тыблицы-order_states,orders,warehouse

---
+++ server/controllers/order_statesController.js
@@ -0,0 +1,97 @@
+'use strict';
+
+//подключаем ништяки
+const {selectQueryBuilder, appendWhere, appendOrderBy} = require('../queryBuilders');
+const {query} = require('../connection')
+const {bodyNormalizator, ASC, DESC} = require('../utils');
+
+//формирование строки запроса на выборку из таблицы с состояниями товара
+let selectQueryText = selectQueryBuilder('store.order_states');
+selectQueryText = appendOrderBy(selectQueryText, {state_name: ASC});
+
+//функция, которая реализует один из методов HTTP - запроса
+exports.get = async (req, res) =>
+    //1.делаем запрос на выборку с помощью ранее сформированной строки
+    query(selectQueryText)
+        .then(results => {
+            //2.при успешной отработке в этом блоке берём данные
+            var rows = [];
+            if (results.rows.length > 0 && results.rows)
+                rows = results.rows.map(row => ({id: row.state_name, ...row}))
+            var response = {
+                success: true,
+                body: {
+                    rows: rows.map(row => {
+                        delete row.state_name;
+                        return row
+                    }),
+                    rowNames: results.fields.map(item => item.name)
+                }
+            }
+            //3.Отправляем их клиенту в формате .json
+            res.json(response);
+        })
+        .catch(err => {
+            //4. Если у нас что-то сломалось, то выводим в консоль на сервере ошибку
+            console.error(err);
+            console.log(`${err.name} code: ${err.code}`);
+            //5. И в ответе говорим клиенту, что всё плохо.
+            res.json({
+                success: false,
+                body: {
+                    error: err.name
+                }
+            })
+        });
+
+
+exports.put = async (req, res) => {
+    console.log(req.body)
+    query("INSERT INTO store.order_states (state_name) VALUES ($1)", req.body)
+        .then(result =>
+            res.json({
+                success: true
+            })
+        )
+        .catch(err => {
+                console.error(err);
+                res.json({
+                    success: false
+                })
+            }
+        )
+}
+
+exports.delete = async (req, res) => {
+    console.log(req.params)
+    query("DELETE FROM store.order_states where state_id = $1", req.params)
+        .then(result =>
+            res.json({
+                success: true
+            })
+        )
+        .catch(err => {
+                console.error(err);
+                res.json({
+                    success: false
+                })
+            }
+        )
+}
+
+exports.update = async (req, res) => {
+    console.log(req.body)
+    query("UPDATE store.order_states SET state_name = $2  WHERE state_id = $1", req.body)
+        .then(result =>
+            res.json({
+                success: true
+            })
+        )
+        .catch(err => {
+                console.error(err);
+                res.json({
+                    success: false
+                })
+            }
+        )
+}
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -7,6 +7,9 @@ module.exports = function (app) {
 
     let conditionsController = require('../controllers/conditionsController');
     let salesController = require('../controllers/salesController');
+    let order_statesController = require('../controllers/order_statesController');
+    let ordersController = require('../controllers/ordersController');
+    let warehouseController = require('../controllers/warehouseController');
     let salesProductsController = require('../controllers/salesProductsController');
 
     app.route('/login')
@@ -24,6 +27,21 @@ module.exports = function (app) {
         .put(salesController.put)
         .post(salesController.update);
 
+    app.route('/order_states')
+        .get(order_statesController.get)
+        .put(order_statesController.put)
+        .post(order_statesController.update);
+
+    app.route('/orders')
+        .get(ordersController.get)
+        .put(ordersController.put)
+        .post(ordersController.update);
+
+    app.route('/warehouse')
+        .get(warehouseController.get)
+        .put(warehouseController.put)
+        .post(warehouseController.update);
+
     app.route('/sales/:id')
         .delete(salesController.delete)
 
@@ -34,6 +52,16 @@ module.exports = function (app) {
 
     app.route('/sales_products/:id')
         .delete(salesProductsController.delete)
+
+    app.route('/order_states/:id')
+        .delete(order_statesController.delete)
+
+    app.route('/orders/:id')
+        .delete(ordersController.delete)
+
+    app.route('/warehouse/:id')
+        .delete(warehouseController.delete)
+
 
     //Если мы исползьуем делит, то можем в адресной строке сказать, какой ид нужно удалить
     //URL выглядит так:http://localhost:3002/conditions/1 с методом DELETE
