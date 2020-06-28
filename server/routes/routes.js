3eafbddb6e08f568c19ac654d3ae21ba4f53fb7c
bagrova-nastya <fefhwjfbwejhf@gmail.com>
2020-06-28 18:56:19 +0300

    Создан контроллеры для таблицы products  и manufacturers. Добавлены маршруты таблицы в routes.js.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -8,12 +8,9 @@ module.exports = function (app) {
     let conditionsController = require('../controllers/conditionsController');
     let salesController = require('../controllers/salesController');
     let productCategoryController = require('../controllers/productCategoryController');
-    let order_statesController = require('../controllers/order_statesController');
-    let ordersController = require('../controllers/ordersController');
-    let warehouseController = require('../controllers/warehouseController');
-    let salesProductsController = require('../controllers/salesProductsController');
     let productsController = require('../controllers/productsController');
     let manufacturersController = require('../controllers/manufacturersController');
+
     app.route('/login')
         .post(conditionsController.login);
 
@@ -28,29 +25,6 @@ module.exports = function (app) {
         .get(salesController.get)
         .put(salesController.put)
         .post(salesController.update);
-
-    app.route('/product_category')
-        .get(productCategoryController.get)
-        .put(productCategoryController.put)
-        .post(productCategoryController.update);
-
-    app.route('/conditions/:id')
-        .delete(conditionsController.delete);
-
-    app.route('/order_states')
-        .get(order_statesController.get)
-        .put(order_statesController.put)
-        .post(order_statesController.update);
-
-    app.route('/orders')
-        .get(ordersController.get)
-        .put(ordersController.put)
-        .post(ordersController.update);
-
-    app.route('/warehouse')
-        .get(warehouseController.get)
-        .put(warehouseController.put)
-        .post(warehouseController.update);
 
     app.route('/product_category')
         .get(productCategoryController.get)
@@ -76,20 +50,6 @@ module.exports = function (app) {
     app.route('/product_category/:id')
         .delete(productCategoryController.delete);
 
-    app.route('/order_states/:id')
-        .delete(order_statesController.delete)
-
-    app.route('/orders/:id')
-        .delete(ordersController.delete)
-
-    app.route('/warehouse/:id')
-        .delete(warehouseController.delete)
-
-
-    app.route('/sales_products')
-        .get(salesProductsController.get)
-        .put(salesProductsController.put)
-        .post(salesProductsController.update);
     app.route('/products/:id')
         .delete(productsController.delete);
 
@@ -98,7 +58,5 @@ module.exports = function (app) {
     //Если мы исползьуем делит, то можем в адресной строке сказать, какой ид нужно удалить
     //URL выглядит так:http://localhost:3002/conditions/1 с методом DELETE
 
-    app.route('/conditions/:id')
-        .delete(conditionsController.delete);
 
 };
