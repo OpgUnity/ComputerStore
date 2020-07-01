73e645d58e9edb2a8830c75e88761735d0152417
Ivan <eroshin_01@mail.ru>
2020-07-01 12:34:04 +0300

    Переделал сортировку и выбор элементов на redux

--- src/const.js
+++ src/const.js
@@ -3,14 +3,14 @@ import {deleteAction, getAction, login, putAction} from "./actions";
 export const SERVER = 'http://localhost:3002';
 export const CONDITION_PATHNAME = '/conditions';
 export const LOGIN_PATHNAME = '/login';
-export const PRODUCT_CATEGORY_PATHNAME='/product_category'
-export const SALES_PATHNAME='/sales'
-export const ORDER_STATES_PATHNAME='/order_states'
-export const ORDERS_PATHNAME='/orders'
-export const WAREHOUSE_PATHNAME='/warehouse'
-export const PRODUCTS_PATHNAME='/products'
-export const MANUFACTURERS_PATHNAME='/manufacturers'
-export const SALES_PRODUCTS_PATHNAME='/sales_products'
+export const PRODUCT_CATEGORY_PATHNAME='/product_category';
+export const SALES_PATHNAME='/sales';
+export const ORDER_STATES_PATHNAME='/order_states';
+export const ORDERS_PATHNAME='/orders';
+export const WAREHOUSE_PATHNAME='/warehouse';
+export const PRODUCTS_PATHNAME='/products';
+export const MANUFACTURERS_PATHNAME='/manufacturers';
+export const SALES_PRODUCTS_PATHNAME='/sales_products';
 
 
 
@@ -50,6 +50,7 @@ export const handleCreate = (item,pathname) => {
         })
         .catch(err => alert(err))
 }
+
 export const handleUpdate = (value,pathname) => {
     fetch(`${SERVER}${pathname}`, {
         method: 'POST',
